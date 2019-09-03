884097d776dc970df8c002d265dcf406f2fb0972
ayortanli <ahmetyortanli@gmail.com>
2019-09-03 12:19:24 +0300

    vendor code splitting in output files

--- ToDoAppClient/webpack.config.js
+++ ToDoAppClient/webpack.config.js
@@ -39,5 +39,28 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
     }
 };
